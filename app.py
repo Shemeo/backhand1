8fe967631a662e594d71a86361e542df7a848e3b
Shemeo <53051853+Shemeo@users.noreply.github.com>
2023-06-23 23:05:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,3 +29,27 @@ def homepage():
 @app.route('/hello', methods=['GET'])
 def hello():
     return "Hello"
+
+@app.route('/api/xrp-usdt-price')
+def get_xrp_usdt_price():
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    parameters = {
+        'symbol': 'XRP',
+        'convert': 'USDT'
+    }
+    headers = {
+        'X-CMC_PRO_API_KEY': 'YOUR_API_KEY'
+    }
+    response = requests.get(url, params=parameters, headers=headers)
+    data = response.json()
+    
+    # Extract the XRP to USDT price from the response
+    if 'XRP' in data['data']:
+        xrp_price_usdt = data['data']['XRP']['quote']['USDT']['price']
+        return jsonify({'xrp_usdt_price': xrp_price_usdt})
+    else:
+        return jsonify({'error': 'Unable to fetch XRP price'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
